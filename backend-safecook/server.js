652accb7fb30f183e536661ed9ecd78506277ada
Pedro Compean Orozco <pedrocompeanorozco@gmail.com>
2026-06-02 23:27:50 -0600

    fix: correcion de ruta de authMiddleware y proteccion de rutas en server.js

--- backend-safecook/server.js
+++ backend-safecook/server.js
@@ -2,37 +2,41 @@ const express = require('express');
 const cors    = require('cors');
 require('dotenv').config();
 
-const app = express(); // creamos la aplicacion
+const app = express(); 
 
 // Middlewares
 app.use(cors());           
 app.use(express.json());   
 
+
+const verificarToken = require('./middlewares/authMiddleware');
+
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
+// Rutas Protegidas 
 const inicioRoutes = require('./routes/inicio');
-app.use('/api/inicio', inicioRoutes);
+app.use('/api/inicio', verificarToken, inicioRoutes);
 
 // Rutas de la Bitácora Histórica
 const bitacoraRoutes = require('./routes/bitacora');
-app.use('/api/bitacora', bitacoraRoutes);
+app.use('/api/bitacora', verificarToken, bitacoraRoutes);
 
 // Rutas de Soporte y Feedback
 const soporteRoutes = require('./routes/soporte');
-app.use('/api/soporte', soporteRoutes);
+app.use('/api/soporte', verificarToken, soporteRoutes);
 
 // Rutas de Detección de Gas
 const deteccionRoutes = require('./routes/deteccion');
-app.use('/api/deteccion', deteccionRoutes);
+app.use('/api/deteccion', verificarToken, deteccionRoutes);
 
 // Rutas de Analítica Predictiva
 const analiticaRoutes = require('./routes/analitica');
-app.use('/api/analitica', analiticaRoutes);
+app.use('/api/analitica', verificarToken, analiticaRoutes);
 
 // Rutas de Usuarios (CRUD Admin)
 const usuariosRoutes = require('./routes/usuarios');
-app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/usuarios', verificarToken, usuariosRoutes);
 
 // Arrancamos el servidor
 const PORT = process.env.PORT || 3000;
